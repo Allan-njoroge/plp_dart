45799705b184abed24164f8110d4f81433bba0f8
Allan-njoroge <allandavis254@gmail.com>
2024-03-04 13:05:34 +0300

    Update

--- data_types.dart
+++ data_types.dart
@@ -1,1 +1,22 @@
-void main() {}
+void main() {
+  String name = "Allan Njoroge";
+  int age = 19;
+  double weight = 65.5;
+
+  List<String> fruits = ["Banana", "Orange", "Apple"];
+
+  Map<String, int> cars = {
+    "Toyota": 10,
+    "Audi": 20,
+    "Merceded": 30,
+  };
+
+  print("My name is $name");
+  print("I am $age years old");
+  print("I weigh $weight Kilograms");
+  print("My favourite fruits are $fruits");
+
+  cars.forEach((key, value) {
+    print("Cars available: $key, Units available: $value");
+  });
+}
